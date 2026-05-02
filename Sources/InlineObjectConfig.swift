c6eb61607bffa9a74cd5ca6ec873733b7be15eb9
Frizlab <fload@me.com>
2026-05-03 01:06:10 +0200

    Add support for async object creator and modifier

--- Sources/InlineObjectConfig.swift
+++ Sources/InlineObjectConfig.swift
@@ -5,6 +5,7 @@ import Foundation
 infix operator --:  BitwiseShiftPrecedence
 infix operator --?: BitwiseShiftPrecedence
 
+
 public func --<T>(_ objectCreator: @autoclosure () throws -> T, _ modifierBlock: (inout T) throws -> Void) rethrows -> T {
 	var res = try objectCreator()
 	try modifierBlock(&res)
@@ -16,5 +17,20 @@ public func --?<T>(_ objectCreator: @autoclosure () throws -> T?, _ modifierBloc
 		return nil
 	}
 	try modifierBlock(&res)
+	return res
+}
+
+
+public func --<T>(_ objectCreator: @autoclosure () async throws -> T, _ modifierBlock: (inout T) async throws -> Void) async rethrows -> T {
+	var res = try await objectCreator()
+	try await modifierBlock(&res)
+	return res
+}
+
+public func --?<T>(_ objectCreator: @autoclosure () async throws -> T?, _ modifierBlock: (inout T) async throws -> Void) async rethrows -> T? {
+	guard var res = try await objectCreator() else {
+		return nil
+	}
+	try await modifierBlock(&res)
 	return res
 }
